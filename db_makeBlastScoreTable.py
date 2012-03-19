d78d60f983e0f51c89ab72064f8b802803e759a7
Matthew Benedict <mnb_0000@hotmail.com>
2012-03-19 14:54:21 -0500

    Re-implemented scoring scheme function - not everything is in there but the bit score norms are

--- db_makeBlastScoreTable.py
+++ db_makeBlastScoreTable.py
@@ -14,17 +14,28 @@
 # -m maxbit: Bit score / max( query self-bit, target self-bit)
 # -m avgbit: Bit score * 2 / (query self-bit + target self-bit)
 
+def minbit(bitscore, qselfbit, tselfbit):
+    return float(bitscore)/min( float(qselfbit), float(tselfbit) )
+
+def maxbit(bitscore, qselfbit, tselfbit):
+    return float(bitscore)/max( float(qselfbit), float(tselfbit) )
+
+def avgbit(bitscore, qselfbit, tselfbit):
+    return float(bitscore) * 2 / (float(qselfbit) + float(tselfbit) )
+
 import optparse, fileinput
 
 validmethods = ['minbit', 'maxbit', 'avgbit']
 
 parser = optparse.OptionParser()
 parser.add_option("-m", "--method", help="Method name", action="store", type="str", dest="method", default=None)
-parser.add_option("-c", "--cutoff", help="Score cutoff to use", action="store", type="float", dest="method", default=None)
+parser.add_option("-c", "--cutoff", help="Score cutoff to use", action="store", type="float", dest="cutoff", default=None)
 (options, args) = parser.parse_args()
 
 if not options.method in validmethods:
     print "ERROR: Invalid method passed to db_makeBlastScoreTable."
+    print "Passed method:"
+    print options.method
     print "Current valid methods are:"
     print validmethods
     exit(2)
@@ -32,4 +43,21 @@ if not options.method in validmethods:
 for line in fileinput.input("-"):
     spl = line.strip().split("\t")
     # Required information
-    bitscore = spl[
+    bitscore = spl[11]
+    qselfbit = spl[12]
+    tselfbit = spl[13]
+
+    qgene = spl[0]
+    tgene = spl[1]
+
+    if options.method == 'minbit':
+        score = minbit(bitscore, qselfbit, tselfbit)
+    elif options.method == 'maxbit':
+        score = maxbit(bitscore, qselfbit, tselfbit)
+    elif options.method == 'avgbit':
+        score = avgbit(bitscore, qselfbit, tselfbit)
+
+    if score < options.cutoff:
+        score = 0
+
+    print "%s\t%s\t%1.3f" % (qgene, tgene, score)

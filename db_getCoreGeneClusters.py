8aeb439af27f9d38bc17004384eeaac26c36fd9b
Matthew Benedict <mnb_0000@hotmail.com>
2012-03-20 10:11:23 -0500

    Complete rewrite of the get core gene cluster function. The function now takes a list of organisms and attempts to find core genes from ALL cluster runs in the database. Also, according to the chosen direction we now can take a large cluster run and prune it directly if desired.

--- db_getCoreGeneClusters.py
+++ db_getCoreGeneClusters.py
@@ -1,65 +1,74 @@
 #!/usr/bin/python
 
-# Given a list of run IDs (by default in column 1)
-# generates a list of run IDs | cluster IDs | gene IDs | Annotations
-# (only the first two are needed to pipe to other commands)
+# This is a pipe command.
 #
-# Options: -r (--rcolumn) - column number starting from 1 for run ID (default: 1)
+# Pipe in a list of organisms (obtain e.g. by grepping the "organisms" file for
+# the subset of organisms that you want)
+#
+# The function first exclude any organisms NOT in the list from the clusters
+# in all of the runs. It then generates a list of core clusters according to our
+# definition:
+#
+# Core gene cluster is defined as a cluster containing exactly one gene from each organism.
+#
 
-import sqlite3, fileinput, optparse
+import sqlite3, fileinput, re
 
-parser = optparse.OptionParser()
-parser.add_option("-r", "--rcolumn", help="Column number (start from 1) for run ID", action="store", type="int", dest="runcolumn", default=1)
-(options, args) = parser.parse_args()
-rc = options.runcolumn - 1 # Convert to Pythonic indexes                                                                                                                                                      
+# FIXME: Option for organism column
 
 con = sqlite3.connect("db/methanosarcina")
 cur = con.cursor()
 
+oc = 0
+
+orgs = []
 for line in fileinput.input("-"):
-
     spl = line.strip().split("\t")
-    # Pull out organisms present in the specified clustering run.
-    query = "CREATE TEMPORARY TABLE desiredorgs AS SELECT DISTINCT organism FROM distinctorgs WHERE runid = ?;"
-    cur.execute(query, (spl[rc],))
+    orgs.append(spl[oc])
 
-    # Generates a table of all the genes for the desired organisms
-    cur.execute("CREATE TEMPORARY TABLE orggenes AS SELECT * FROM processed INNER JOIN desiredorgs ON desiredorgs.organism = processed.organism;")
-    
-    # Generate unique combinations of run IDs, cluster IDs, and organisms for which the organism matches an organism we want
-    # and the run ID matches the one we're using at the moment.
-    #
-    # This combined with the next command will ensure that we only get clusters that contain ALL and ONLY the organisms that we're interested in...
-    cur.execute("""CREATE TEMPORARY TABLE unqclusterorgs AS
-                      SELECT DISTINCT clusters.runid AS runid, clusters.clusterid AS clusterid, orggenes.organism as organism FROM clusters                                                               
-                      INNER JOIN orggenes ON orggenes.geneid = clusters.geneid 
-                      INNER JOIN desiredorgs on orggenes.organism = desiredorgs.organism
-                      WHERE runid=?;""", (spl[rc], ))
+cur.execute("DROP TABLE IF EXISTS myorganisms;")
+cur.execute("DROP TABLE IF EXISTS clusterorgs_my;")
 
-    cur.execute("""CREATE TEMPORARY TABLE clusterids AS 
-                    SELECT clusterid FROM unqclusterorgs 
-                    GROUP BY clusterid HAVING count(*)=(SELECT count(desiredorgs.organism) FROM desiredorgs)
-                   
-                    INTERSECT
- 
-                    SELECT clusterid FROM clusters
-                    WHERE runid=?
-                    GROUP BY clusterid HAVING count(*)=(SELECT count(desiredorgs.organism) FROM desiredorgs);""", (spl[rc], ) )
+# Desired organism table
+cur.execute("CREATE TABLE myorganisms(organism VARCHAR(256));")
+for org in orgs:
+    cur.execute("INSERT INTO myorganisms VALUES (?);", (org, ) )
 
-    cur.execute("""SELECT clusters.runid, clusters.clusterid, clusters.geneid, processed.annotation
-                      FROM clusters
-                      INNER JOIN processed ON processed.geneid = clusters.geneid
-                      INNER JOIN clusterids ON clusters.clusterid = clusterids.clusterid
-                      WHERE clusters.runid=?;""", (spl[rc], ))
+# Filter clusterorgs by which organism we want
+query = """CREATE TABLE clusterorgs_my AS
+                SELECT * FROM clusterorgs
+                WHERE clusterorgs.organism IN myorganisms;"""
 
+cur.execute(query)
+
+# Speedup
+cur.execute("CREATE INDEX myorgrunidx ON clusterorgs_my(runid);")
+cur.execute("CREATE INDEX myorgclusteridx ON clusterorgs_my(clusterid);")
+
+checkPairs = []
+cur.execute("SELECT * from clusterorgs_my;")
+for l in cur:
+    s = list(l)
+    checkPairs.append( [s[0], s[1]] )
+
+for p in checkPairs:
+    cur.execute("""SELECT geneid FROM clusterorgs_my
+                    WHERE clusterorgs_my.runid = ?
+                     AND clusterorgs_my.clusterid = ?;""",
+                (p[0], p[1]) )
+    genelist = set()
+    orglist =set()
     for l in cur:
         s = list(l)
-        stri = "\t".join(str(t) for t in s)
-        print stri
+        genelist.add(l[0])
+        # Remove peg.#### from the names of the genes to get organism list
+        orglist.add(re.sub("\.peg\.\d+", "", l[0]))
 
-    cur.execute("DROP TABLE clusterids;")
-    cur.execute("DROP TABLE desiredorgs;")
-    cur.execute("DROP TABLE orggenes;")
-    cur.execute("DROP TABLE unqclusterorgs;")
+    # Definition of core gene
+    if len(genelist) == len(orglist) and len(genelist) == len(orgs):
+        print "%s\t%s" % (p[0], p[1])
+
+cur.execute("DROP TABLE myorganisms;")
+cur.execute("DROP TABLE clusterorgs_my;")
 
 con.close()

d9f3d7ea601433bf90947bb54e8c08b370f36939
Matthew Benedict <mnb_0000@hotmail.com>
2012-03-28 15:33:08 -0500

    Changed runID to reflect how the clusters were created - they will always be the same if the clusters are created the same way and should now be easier to search through

--- flattenClusterFile.py
+++ flattenClusterFile.py
@@ -4,6 +4,14 @@
 # 
 # Pipe in the cluster file (from MCL)
 # and the flattened file comes out in stdout
+# You must provide this function what parameters you used to generate
+# the clusters:
+#
+# - inflation
+# - distance metric
+# - cutoff
+#
+# All of these will go into the run ID.
 #
 # The "flattened file" contains only three columns:
 # - A run ID (which should be unique)
@@ -14,18 +22,28 @@
 # (e.g. give me the blast results for all of the genes listed in cluster 1
 # against anything else)
 
-import fileinput, random, string
+import fileinput
+import optparse
 
-# 64 random bits should be enough to uniquely distinguish two runs...
-# If we have a cluster ID we can then determine by SQL query what organisms were
-#    utilized in that cluster if needed.
-clusterid = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(64))
+parser = optparse.OptionParser()
+parser.add_option("-I", "--inflation", help="Inflation parameter used for MCL clustering", action="store", type="float", dest="inflation", default=None)
+parser.add_option("-c", "--cutoff", help="Scoring cutoff used for MCL clustering", action="store", type="float", dest="cutoff", default=None)
+parser.add_option("-m", "--method", help="Scoring method used for MCL clustering", action="store", type="string", dest="method", default=None)
+(options, args) = parser.parse_args()
 
+if options.method == None or options.cutoff == None or options.inflation == None:
+    print "ERROR in flattenClusterFile: -I, -c, and -m are all required"
+    exit(2)
+
+runid = "I_%1.2f_cutoff_%1.2f_method_%s" %(options.inflation, options.cutoff, options.method)
+if len(runid) > 64:
+    print "WARNING: Resulting clusterID is too long - will truncate to 64 characters"
+    runid = runid[0:64]
 
 rowIdx = 1
 for line in fileinput.input("-"):
     spl = line.strip().split('\t')
     for gene in spl:
-        string = str(clusterid) + "\t" + str(rowIdx) + "\t" + gene
+        string = str(runid) + "\t" + str(rowIdx) + "\t" + gene
         print string
     rowIdx = rowIdx + 1
--- getClusterFastas.py
+++ getClusterFastas.py
@@ -6,7 +6,7 @@
 #
 # Organism name and annotation are placed in "labelfolder"
 #
-# Files outputted with first 10 digits of run ID followed by the cluster ID
+# Files outputted with run ID followed by the cluster ID
 #
 ##############
 # WARNING!!!!!
@@ -39,8 +39,8 @@ for line in s:
         if not labelfid == -1:
             labelfid.close()
         
-        fid = open(os.path.join(outputfolder, myrunid[0:9] + "_" + spl[1] + ".fasta"), "w")
-        labelfid = open(os.path.join(labelfolder, myrunid[0:9] + "_" + spl[1] + ".fasta_aln_newick.info"), "w")
+        fid = open(os.path.join(outputfolder, myrunid + "_" + spl[1] + ".fasta"), "w")
+        labelfid = open(os.path.join(labelfolder, myrunid + "_" + spl[1] + ".fasta_aln_newick.info"), "w")
         lastone = spl[1]
     
     fid.write(">" + spl[2] + " " + spl[4] + "\n")

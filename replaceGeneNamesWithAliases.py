3c61a17b6f4f4fb530a10545422b0fc29fcec7dd
Matthew Benedict <mattb112885@autorecon.(none)>
2012-05-22 09:30:01 -0500

    Add utility to replace gene names with aliases (from the geneinfo file) - useful for making newick files with genes labeled with their annotations

---
+++ replaceGeneNamesWithAliases.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+# This is a pipe command.
+# Pipe in a newick file
+#
+# The input argument is the "geneinfo" table containing gene IDs in the first column
+# and what to replace them with in the second column.
+#
+# The output is a new newick file with 
+# the gene names replaced (after sanitizing)
+
+import sys
+import fileinput
+
+if not len(sys.argv) == 2:
+    print "Usage: replaceGeneInNewick [Replacement table]"
+    exit(2)
+
+newickString = ''.join( [ line.strip() for line in fileinput.input("-") ] )
+
+geneToAnnotation = {}
+for line in open(sys.argv[1], "r"):
+    spl = line.strip().split("\t")
+    geneToAnnotation[spl[0]] = spl[1]
+
+# Remove special characters that can confound the newick parser from the annotation strings.
+# Replace with underscores
+charToRemove = " \t():,'\""
+for gene in geneToAnnotation:
+    for char in charToRemove:
+        geneToAnnotation[gene] = geneToAnnotation[gene].replace(char, "_")
+
+# Actually replace the annotations now
+for gene in geneToAnnotation:
+    newickString = newickString.replace(gene, geneToAnnotation[gene])
+
+print newickString
+    
+

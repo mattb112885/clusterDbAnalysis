556423fb909fbf4e4d968d02ca9ad6bfd746395e
Matthew Benedict <mattb112885@autorecon.(none)>
2012-05-28 11:07:43 -0500

    Add function to add aliases to gene names so I can more easily incorporate carefully-curated \ common names of genes and see what things are

---
+++ addAliasesToGeneNames.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+# This is a pipe command
+#
+# Pipe in a data file containing annotations
+# and provide the aliases file
+# ( PEG ID | Alias)
+#
+#
+# Will add aliases to the gene name (separated by underscores)
+# in order specified in the aliases file.
+#
+# By default expects a RAW file (gene 
+
+import optparse
+import fileinput
+
+usage = "%prog [options] aliasFile"
+parser = optparse.OptionParser(usage=usage)
+parser.add_option("-a", "--annotecolumn", help="Column number for annotation (start from 1)", action="store", type="int", dest="annotec", default=8)
+parser.add_option("-g", "--geneidcolumn", help="Column number for gene id (start from 1)", action="store", type="int", dest="genec", default=2)
+(options, args) = parser.parse_args()
+
+if len(args) == 0:
+    print "ERROR: in %prog: Alias file must be provided"
+    exit(2)
+
+annoteColumn = options.annotec - 1
+geneColumn = options.genec - 1
+
+geneToAliases = {}
+for line in open(args[0], "r"):
+    spl = line.strip().split("\t")
+    if spl[0] in geneToAliases:
+        geneToAliases[spl[0]] += "_%s" %(spl[1])
+    else:
+        geneToAliases[spl[0]] = "_%s" %(spl[1])
+
+for line in fileinput.input("-"):
+    spl = line.strip().split("\t")
+    if spl[geneColumn] in geneToAliases:
+        spl[annoteColumn] += geneToAliases[spl[geneColumn]]
+    print "\t".join(spl)

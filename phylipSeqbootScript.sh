9eaae8d95e41496607fcd00e5858fb5e522e8df9
Matthew Benedict <mattb112885@autorecon.(none)>
2012-06-06 16:54:01 -0500

    Fix phylip seqboot script to accept number of bootstraps as an input argument...

--- phylipSeqbootScript.sh
+++ phylipSeqbootScript.sh
@@ -3,22 +3,28 @@
 # This is a script to run phylip
 # seqboot non-interactively
 
-if [ $# -ne 2 ]; then
-    echo "USAGE: phylipSeqbootScript.sh [phylip_file] [out_file]";
+if [ $# -ne 3 ]; then
+    echo "USAGE: phylipSeqbootScript.sh [phylip_file] [out_file] [N_replicates]";
     exit 0;
 fi
 
 infile=$1;
 outfile=$2
+reps=$3
 
-# SEQBOOT asks for a filename first
+# SEQBOOT asks for a filename first (this script assumes you did NOT name the file "infile" like it looks for by default)
 str1=${infile}
-# Are the settings acceptable?
-str2="Y"
+# Are the settings acceptable? - No, we want to specify the number of replicates.
+str2="R"
+# This is the number we want
+str3=${reps}
+# The settings are now acceptable
+str4="Y"
 # Random number seed
-str3="12345"
+str5="12345"
 
-echo ${str1}'\n'${str2}'\n'${str3}'\n' | phylip seqboot
+# Redirect to /dev/null because this will be called as part of a pipe command.
+echo ${str1}'\n'${str2}'\n'${str3}'\n'${str4}'\n'${str5} | phylip seqboot > /dev/null
 
 # Move the "outfile" to the desired location
 mv outfile ${outfile}

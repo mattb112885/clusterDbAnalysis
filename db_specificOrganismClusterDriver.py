bbb10ed24f7e0feb65f2414a6c06d00f9a560787
Matthew Benedict <mattb112885@autorecon.(none)>
2012-06-09 13:59:53 -0500

    Fixed a couple of nuisances

--- db_specificOrganismClusterDriver.py
+++ db_specificOrganismClusterDriver.py
@@ -12,11 +12,13 @@
 #
 # Put the results in a file with the names of all the parts of the group
 # separated by "_"
+#
+# Note - this function itself doesn't specifically call the database but depends on two functions that do.
 
 import os, sys
 
 if not len(sys.argv) == 5:
-    print "Usage> ./db_specificOrganismClusterDriver.py [groupfile] [Inflation] [cutoff] [scoremethod]"
+    sys.stderr.write("Usage: ./db_specificOrganismClusterDriver.py [groupfile] [Inflation] [cutoff] [scoremethod]\n")
     exit(2)
 
 groupfile = sys.argv[1]
@@ -34,7 +36,14 @@ for line in fid:
     safewhole = " ".join(s for s in safepart)
 
     # Generate a filename - no quotes needed or wanted here but we do need to replace spaces
-    foutname = "clusters/" + "_".join(s.replace(" ", "_").replace("!","_NOT_") for s in spl ) + "_I_" + str(inflation) + "_c_" + str(cutoff) + "_m_" + str(method)
+    orgstr = "_".join(s.replace(" ", "_").replace("!","NOT_") for s in spl)
+    foutname = "clusters/%s_I_%s_c_%s_m_%s" %(orgstr, inflation, cutoff, method)
+
+    # Check for filename length. If the file name is too long (255 character limit in modern windows, OSX, and unix), cut it off and warn the user
+                     
+    if len(foutname) > 255:
+        sys.stderr.write("WARNING: resulting file name %s is too long and will be truncated to 255 characters. This could cause naming conflicts between run IDs...\n" %(foutname))
+        foutname = foutname[0:255]
 
     # Make sure that file doesn't exist already. If it does, skip over it... mcl takes a long time.
     try:
@@ -45,5 +54,5 @@ for line in fid:
         cmd = ("python src/db_getBlastResultsBetweenSpecificOrganisms.py " + safewhole + 
                " | python src/db_makeBlastScoreTable.py -m " + str(method) + " -c " + str(cutoff) +
                " | mcl - --abc -I " + str(inflation) + " -o " + foutname + ";")
-        print cmd
+        sys.stderr.write("%s\n" %(cmd) )
         os.system(cmd)

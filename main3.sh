b2aefcf497a90d942b3e657cd0f9c1b799d9857b
Matthew Benedict <mattb112885@autorecon.(none)>
2012-06-10 11:58:03 -0500

    Add main3 - this file isnt used right now but I might start with it again later

---
+++ main3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+NCORES=8
+
+# Second part of the pipeline:
+#
+# We have clusters, so now how do we identify the pseudogenes?
+#  / curate the clusters
+# The answer, tentatively, is with TBLASTN
+#
+
+# Requirements:
+# Biopython (for parsing the genbank files - this can be skipped if you have
+#      nucleotide fasta files already)
+# BLAST+ (tblastn)
+
+mkdir tblastn 2> /dev/null;
+mkdir tblastn_cutoff 2> /dev/null;
+mkdir genomefasta 2> /dev/null;
+
+echo "Converting genbank files to full-nucleotide fasta files...";
+
+cd genbank;
+for file in *; do
+    cat ${file} | python ../src/genbank2nucleotides.py > ../genomefasta/${file}.fna 2> /dev/null;
+done
+cd ..;
+
+echo "Running tblastn...";
+python src/tblastn_all_vs_all.py faa/ genomefasta/ tblastn/ ${NCORES};
+
+echo "tblastn done. processing (removing very weak hits)..."
+cd tblastn;
+for file in *; do
+    cat ${file} | python ../src/processTblastResults.py 50 > ../tblastn_cutoff/${file}.cut;
+done
+cd ..;
+
+echo "Done. Concatinating tblastn results..."
+cat tblastn_cutoff/* > db/tblastn_cat;
+
+echo "Done. Adding TBLASTN information to the database... (this takes a long time)"
+sqlite3 db/methanosarcina < src/builddb_3.sql

d108e1a1bb0c2837eb4ee7ff0b040af4fa34979d
Matthew Benedict <benedic1@biome.life.illinois.edu>
2012-08-21 10:27:59 -0500

    Remove obsolete requirement for label folder and clean up string processing in getClusterFastas.py

--- src/getClusterFastas.py
+++ src/getClusterFastas.py
@@ -22,34 +22,29 @@ import sys, os
 import fileinput
 import optparse
 
-usage = "%prog [fastafolder] [labelfolder] < cluster_info_file"
+usage = "%prog [fastafolder] < cluster_info_file"
 description="Generate a FASTA file for each cluster present in the specified clusterinfo file (as generated from e.g. db_getClusterGeneInfo). The file must be in order by cluster"
 parser = optparse.OptionParser(usage=usage, description=description)
 (options, args) = parser.parse_args()
 
-if not len(args) == 2:
-    sys.stderr.write("ERROR: Both fastafolder and labelfolder are required (see -h for usage details)\n")
+if not len(args) == 1:
+    sys.stderr.write("ERROR: Fastafolder is a required argument (see -h for usage details)\n")
     exit(2)
 
 outputfolder = args[0]
-labelfolder = args[1]
 
 lastone = None
 fid = -1
-labelfid = -1
 for line in fileinput.input("-"):
     spl = line.strip('\r\n').split("\t")
     myrunid = spl[0]
     if not spl[1] == lastone:
         if not fid == -1:
             fid.close()
-        if not labelfid == -1:
-            labelfid.close()
-        
-        fid = open(os.path.join(outputfolder, myrunid + "_" + spl[1] + ".fasta"), "w")
-        labelfid = open(os.path.join(labelfolder, myrunid + "_" + spl[1] + ".fasta_aln_newick.info"), "w")
+        fname = "%s_%s.fasta" %(myrunid, spl[1])
+        fid = open(os.path.join(outputfolder, fname), "w")
         lastone = spl[1]
-    
-    fid.write(">" + spl[2] + " " + spl[4] + "\n")
-    fid.write(spl[5] + "\n")
-    labelfid.write(spl[2] + "\t" + spl[3] + " " + spl[4] + "\n")
+    # Write to fasta
+    ln = ">%s %s\n%s\n" %(spl[2], spl[4], spl[5])
+    fid.write(ln)
+

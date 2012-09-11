ad1228afb1bd02949a418a7daac2057646961a1c
Matthew Benedict <benedic1@biome.life.illinois.edu>
2012-09-11 13:08:16 -0500

    Make annotation optional in annoteSeq2Fasta.py

--- src/annoteSeq2Fasta.py
+++ src/annoteSeq2Fasta.py
@@ -12,18 +12,23 @@ usage = "%prog [options] < gene_seq_table > fasta_file"
 description = "Turn a table containing gene IDs, annotations, and sequences into a FASTA file"
 parser = optparse.OptionParser(usage=usage, description=description)
 parser.add_option("-g", "--geneid", help="Column number (start from 1) for gene id", action="store", type="int", dest="geneidcol", default=1)
-parser.add_option("-a", "--annote", help="Column number (start from 1) for annotation / FASTA header", action="store", type="int", dest="annotecol", default=2)
+parser.add_option("-a", "--annote", help="Column number (start from 1) for annotation / FASTA header (optional, by default no annotation is included)", action="store", type="int", dest="annotecol", default=None)
 parser.add_option("-s", "--seqcol", help="Column number (start from 1) for sequence column", action="store", type="int", dest="seqcol", default=3)
 (options, args) = parser.parse_args()
 
 geneidcol = options.geneidcol - 1
-annotecol = options.annotecol - 1
 seqcol = options.seqcol - 1
+if options.annotecol is not None:
+    annotecol = options.annotecol - 1
+else:
+    annotecol = None
 
 for line in fileinput.input("-"):
     spl = line.strip('\r\n').split("\t")
     geneid = spl[geneidcol]
-    annote = spl[annotecol]
     seq = spl[seqcol]
-    print ">" + geneid + " " + annote
-    print seq
+    if annotecol is None:
+        annote = ""
+    else:
+        annote = " %s" %(spl[annotecol])
+    print ">%s%s\n%s" %(geneid, annote, seq)

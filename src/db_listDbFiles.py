f35237be054715b4146f27981097fc9ab9b98495
Matthew Benedict <benedic1@biome.life.illinois.edu>
2012-09-20 13:53:54 -0500

    Improve the db_listDbFiles.py function to be more readable...

--- src/db_listDbFiles.py
+++ src/db_listDbFiles.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 
-import os, optparse
-usage = "%prog > filelist"
+import os, optparse, pprint, sys
+
+usage = "%prog [options] > filelist"
 description="List all executable files provided as part of this software package"
 parser = optparse.OptionParser(usage=usage, description=description)
-parser.parse_args()
+parser.add_option("-w", "--maxw", help="Maximum number of characters (D=100)", action="store", type="int", dest="maxw", default=100)
+(options, args) = parser.parse_args()
 
 fileloc = os.path.abspath(__file__)
 path = os.path.dirname(fileloc)
 ls = os.listdir(path)
 
-ln = "\n".join(s for s in sorted(ls))
-print ln
+# Pretty-print into columns
+maxw = options.maxw
+maxl = max( [ len(s) for s in ls ] )
+maxn = int(maxw/maxl)
+c = 0
+for s in sorted(ls):
+    if c >= maxn:
+        sys.stdout.write("\n")
+        c = 0
+    sys.stdout.write(s.ljust(maxl) + "\t")
+    c += 1
+

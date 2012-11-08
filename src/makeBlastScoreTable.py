307d72832001a1c2437a7e139227aa1ec6c76bbc
Matthew Benedict <benedic1@biome.life.illinois.edu>
2012-11-08 12:46:59 -0600

    Cleaning up some unused features...

--- src/makeBlastScoreTable.py
+++ src/makeBlastScoreTable.py
@@ -12,7 +12,6 @@
 #
 # -m minbit: Bit score / min( query self-bit, target self-bit)
 # -m maxbit: Bit score / max( query self-bit, target self-bit)
-# -m avgbit: Bit score * 2 / (query self-bit + target self-bit)
 # 
 # Local alignments (if you WANT domain hits)
 # -m normhsp: Bit score / hsp length (equivalent to r in mcxdeblast)
@@ -23,24 +22,22 @@ def minbit(bitscore, qselfbit, tselfbit):
 def maxbit(bitscore, qselfbit, tselfbit):
     return float(bitscore)/max( float(qselfbit), float(tselfbit) )
 
-def avgbit(bitscore, qselfbit, tselfbit):
-    return float(bitscore) * 2 / (float(qselfbit) + float(tselfbit) )
-
 def normhsp(bitscore, hsplen):
     return float(bitscore)/float(hsplen)
 
 import optparse, fileinput
 from locateDatabase import *
 
-validmethods = ['minbit', 'maxbit', 'avgbit', 'normhsp']
+validmethods = ['minbit', 'maxbit', 'normhsp']
 
 usage="%prog -m [method] -c [cutoff] [options] < blast_result_table"
 description = """Given a blast score table (augmented with self-bit scores for query and target genes),
-calculates a similarity value based on the desired scoring metric. 
-Currently implemented metrics: %s""" %(" ".join(validmethods))
+calculates a similarity value based on the desired scoring metric.
+Returns a three column tab-delimited table: query, target, score"""
 parser = optparse.OptionParser(usage=usage, description=description)
-parser.add_option("-m", "--method", help="Method name", action="store", type="str", dest="method", default=None)
-parser.add_option("-c", "--cutoff", help="Score cutoff to use", action="store", type="float", dest="cutoff", default=None)
+parser.add_option("-m", "--method", help="Homology metric to use for calculation (required). Valid metrics: %s" %(" ".join(validmethods)), 
+                  action="store", type="str", dest="method", default=None)
+parser.add_option("-c", "--cutoff", help="Score cutoff to use (required)", action="store", type="float", dest="cutoff", default=None)
 parser.add_option("-n", "--noprint", help="Set this flag if you do not want to print edges less than the cutoff as zeros (default = False - print those edges", 
                   action="store_true", dest="noprint", default=False)
 (options, args) = parser.parse_args()
@@ -65,8 +62,6 @@ for line in fileinput.input("-"):
         score = minbit(bitscore, qselfbit, tselfbit)
     elif options.method == 'maxbit':
         score = maxbit(bitscore, qselfbit, tselfbit)
-    elif options.method == 'avgbit':
-        score = avgbit(bitscore, qselfbit, tselfbit)
     elif options.method == 'normhsp':
         score = normhsp(bitscore, hsplen)
 

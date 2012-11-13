ca34e2577909c2b2bbd2ebdfd4f7a70221e1482c
Matthew Benedict <mattb112885@autorecon.(none)>
2012-11-12 20:29:04 -0600

    Add a rudamentary parallelized version of RPSBLAST - just does the trivial thing and separates out calls for organisms. Use this version if you cannot get the parallelization that is suppsoed to be native to RPSBLAST to work (like me).

--- main4.sh
+++ main4.sh
@@ -12,10 +12,37 @@ fi
 
 NCORES=$1
 
+mkdir rpsblast_res 2> /dev/null
+mkdir cd_db 2> /dev/null
+
+cd cd_db
+if [ ! -f cdd.tar.gz ]; then
+    echo "DOWNLOADING the conserved protein domain database files..."
+    wget ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cdd.tar.gz
+    # Note - this tar.gz file doesn't create another directory for all the files.
+    tar xzf cdd.tar.gz
+else
+    echo "Conserved-protein database already present in expected location."
+fi
+
+# Gunzip (unlike tar xzf) deletes the gz file so I check for the file inside that.
+if [ ! -f ../db/cddid.tbl ]; then
+    echo "DOWNLOADING conserved protein database info file..."
+    wget ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddid.tbl.gz
+    gunzip cddid.tbl.gz
+    mv cddid.tbl ../db/cddid.tbl
+fi
+
+cd ..
+
 if [ ! -f db/external_CDD ]; then
-    ./computeConservedDomains.sh "CDD" db/allgenomes.faa db/external_CDD "${NCORES}";
+#        ./computeConservedDomains.sh "CDD" db/allgenomes.faa db/external_CDD "${NCORES}";
+    Rpsblast_all_vs_one.py -n "${NCORES}" -c 1E-5 "cd_db/Cdd.pn" "faa/" "rpsblast_res/"
+    cat rpsblast_res/* > db/external_CDD
     cat db/external_CDD | sed -r "s/^(.*?\s+.*?),/\1/g" > db/external_CDD_MOD
     mv db/external_CDD_MOD db/external_CDD
 fi
+
+exit 0;
 
 sqlite3 db/DATABASE.sqlite < src/builddb_4.sql
---
+++ src/Rpsblast_all_vs_one.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import optparse, os, sys
+from ruffus import *
+
+usage = "%prog [options] target_db inputdir outputdir"
+description = """Parallelize RPSBLAST by running each organism in the inputdir
+separately against the same target database."""
+
+parser = optparse.OptionParser(usage=usage, description=description)
+parser.add_option("-n", "--numcores", help="Number of cores to use (D: 1)", 
+                  action="store", type="int", dest="numcores", default=1)
+parser.add_option("-e", "--extension", help="Extension for input fasta files (D: faa)", 
+                  action="store", type="str", dest="extension", default="faa")
+parser.add_option("-c", "--cutoff", help="E-value cutoff (D: 1E-5)", 
+                  action="store", type="float", dest="cutoff", default=1E-5)
+(options, args) = parser.parse_args()
+
+if len(args) != 3:
+    sys.stderr.write("ERROR: target_db, inputdir, and outputdir are necessary input arguments\n")
+    exit(2)
+
+target_db = args[0]
+inputdir = args[1]
+outputdir = args[2]
+
+if not os.path.exists(target_db + ".phr"):
+    # The -S 1 is because NCBI recommended NOT using scaling even
+    # though they set it as the default anyway.
+    # See:
+    #  ftp://ftp.ncbi.nih.gov/blast/documents/rpsblast.html
+    # This is the docs for the old version - they removed the warning
+    # from the new version but didn't explain why...
+    os.system("formatrpsdb -i \"%s\" -S 1" %(target_db))
+
+faa_list = []
+for f in os.listdir(os.path.join(args[1])):
+    if f.endswith(".%s" %(options.extension)):
+        faa_list.append( os.path.join(args[1], f) )
+
+evalue = options.cutoff
+
+arglist = []
+for faa in faa_list:
+    arglist.append( [ faa, target_db, outputdir, evalue ] )
+
+@parallel(arglist)
+def singleRpsBlast(faa, target_db, outputdir, evalue):
+    outfile = os.path.join(outputdir, "%s_rpsout" %(faa))
+    if os.path.exists(outfile):
+        return
+    cmd = "rpsblast -i %s -d %s -o %s -m 8 -e %e -P 1;" %(faa, target_db, outfile, evalue)
+    sys.stderr.write("%s\n" %(cmd))
+    os.system(cmd)
+
+pipeline_run([singleRpsBlast], multiprocess=options.numcores)

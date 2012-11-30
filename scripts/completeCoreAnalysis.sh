dd02b2148edd3bfe07fa27eb37b401c733eca45e
Matthew Benedict <benedic1@biome.life.illinois.edu>
2012-11-30 10:00:19 -0600

    Move core analysis script to scripts dir and update documentation

--- scripts/completeCoreAnalysis.sh
+++ scripts/completeCoreAnalysis.sh
@@ -3,8 +3,26 @@
 # Script to completely do a core genome analysis based on a specific run id
 
 if [ $# -lt 2 ]; then
+    echo ""
     echo "USAGE: completeCoreAnalysis.sh [runid] [rootorg] [NTHREADS (optional)]";
     echo "rootorg should have spaces replaced by underscores"
+    echo ""
+    echo "DESCRIPTION: Performs common tasks needed to compute a core tree,"
+    echo "saving all intermediate files."
+    echo ""
+    echo "Completed tasks:"
+    echo "1. Computation of core gene clusters. Core gene clusters for purposes of"
+    echo "   this script are those with exactly one representative in each organism"
+    echo "   in the cluster run."
+    echo "2. Generation of (protein) FASTA files for each of the core gene clusters."
+    echo "3. Alignment with MAFFT (--auto)"
+    echo "4. Trimming with GBlocks (with the relaxed settings)"
+    echo "5. Concatination of the alignments"
+    echo "6. Treeing with FASTTREE (with local bootstraps as in the default)"
+    echo ""
+    echo "This script can be run from anywhere and will create a lot of files so I suggest"
+    echo "running it from its own designated folder."
+    echo ""
     exit 0;
 fi
 
@@ -60,8 +78,8 @@ if [ ! -f "core_cat_aln_fasttree_${runid}" ]; then
 fi
 
 # Reroot tree
-if [ ! -f "core_cat_fasttree_rerooted_${runid}" ]; then
-    db_displayTree.py -n -b "core_cat_fasttree_rerooted_${runid}" -r "${root}";
+if [ ! -f "core_cat_fasttree_rerooted_${runid}".nwk ]; then
+    db_displayTree.py -n -b "core_cat_fasttree_rerooted_${runid}" -r "${root}" "core_cat_aln_fasttree_${runid}";
 fi
 
 # Get subsets

39201ee3b44947c8144e5df9dd0b95697202c47b
James R. Henriksen <jamesrh@illinois.edu>
2012-12-10 10:45:57 -0600

    In makeBlastScoreTable.py added options and code for 2 additional metrics.  They can be non-symetric, so this is for the tblastn searching not the clustering.

--- src/makeBlastScoreTable.py
+++ src/makeBlastScoreTable.py
@@ -12,6 +12,8 @@
 #
 # -m minbit: Bit score / min( query self-bit, target self-bit)
 # -m maxbit: Bit score / max( query self-bit, target self-bit)
+# -m maxselfbit: Bit score / query self-bit
+# -m avgbit: Bit score * 2 / (query self-bit + target self-bit)
 # 
 # Local alignments (if you WANT domain hits)
 # -m normhsp: Bit score / hsp length (equivalent to r in mcxdeblast)
@@ -22,28 +24,30 @@ def minbit(bitscore, qselfbit, tselfbit):
 def maxbit(bitscore, qselfbit, tselfbit):
     return float(bitscore)/max( float(qselfbit), float(tselfbit) )
 
+def maxselfbit(bitscore, qselfbit):
+    return float(bitscore)/float(qselfbit)
+
+def avgbit(bitscore, qselfbit, tselfbit):
+    return float(bitscore) * 2 / (float(qselfbit) + float(tselfbit) )
+
 def normhsp(bitscore, hsplen):
     return float(bitscore)/float(hsplen)
 
 import optparse, fileinput
 from locateDatabase import *
 
-validmethods = ['minbit', 'maxbit', 'normhsp']
+validmethods = ['minbit', 'maxbit', 'maxselfbit', 'avgbit', 'normhsp']
 
 usage="%prog -m [method] -c [cutoff] [options] < blast_result_table"
-description = """Given a blast score table (augmented with self-bit scores for query and target genes),
-calculates a similarity value based on the desired scoring metric.
-Returns a three column tab-delimited table: query, target, score"""
+description = "Given a blast score table (augmented with self-bit scores for query and target genes), calculates a similarity value based on the desired scoring metric. Currently implemented metrics: %s" %(" ".join(validmethods))
 parser = optparse.OptionParser(usage=usage, description=description)
-parser.add_option("-m", "--method", help="Homology metric to use for calculation (required). Valid metrics: %s" %(" ".join(validmethods)), 
-                  action="store", type="str", dest="method", default=None)
-parser.add_option("-c", "--cutoff", help="Score cutoff to use (required)", action="store", type="float", dest="cutoff", default=None)
-parser.add_option("-n", "--noprint", help="Set this flag if you do not want to print edges less than the cutoff as zeros (default = False - print those edges", 
-                  action="store_true", dest="noprint", default=False)
+parser.add_option("-m", "--method", help="Method name", action="store", type="str", dest="method", default=None)
+parser.add_option("-c", "--cutoff", help="Score cutoff to use", action="store", type="float", dest="cutoff", default=None)
+parser.add_option("-n", "--noprint", help="Set this flag if you do not want to print edges less than the cutoff as zeros (default = False - print those edges", action="store_true", dest="noprint", default=False)
 (options, args) = parser.parse_args()
 
 if not options.method in validmethods:
-    sys.stderr.write("ERROR: Invalid method passed to makeBlastScoreTable.py\n")
+    sys.stderr.write("ERROR: Invalid method passed to db_makeBlastScoreTable\n")
     sys.stderr.write("Current valid methods are:\n%s\n" %("\t".join(validmethods)))
     exit(2)
 
@@ -62,6 +66,10 @@ for line in fileinput.input("-"):
         score = minbit(bitscore, qselfbit, tselfbit)
     elif options.method == 'maxbit':
         score = maxbit(bitscore, qselfbit, tselfbit)
+    elif options.method == 'maxselfbit':
+        score = maxselfbit(bitscore, qselfbit)
+    elif options.method == 'avgbit':
+        score = avgbit(bitscore, qselfbit, tselfbit)
     elif options.method == 'normhsp':
         score = normhsp(bitscore, hsplen)
 

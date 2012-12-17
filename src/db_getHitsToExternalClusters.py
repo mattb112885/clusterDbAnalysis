87cafdb18afa7b85ad070396e5ac26ae4beb5721
Matthew Benedict <benedic1@biome.life.illinois.edu>
2012-12-16 20:20:59 -0600

    Added function to take an external cluster (COG, PFAM, ...) ID and obtain the RPSBLAST hits

---
+++ src/db_getHitsToExternalClusters.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import fileinput
+import optparse
+import sqlite3
+import sys
+
+from locateDatabase import *
+
+usage = "%prog < external_clusterids > similarities"
+description = """Given a list of external clusterIDs (from cog, pfam, tigrfam...), attempts to identify proteins
+in the database that are homologous (by RPSBLAST) to the profile implied by those clusters, and returns them
+and their E-values."""
+
+parser = optparse.OptionParser(usage=usage, description=description)
+
+parser.add_option("-c", "--column", help="Column number for external cluster ID (D=1)",
+                  action = "store", type="int", dest="ecc", default=1)
+parser.add_option("-e", "--evalue", help="Evalue cutoff (D: 1E-5, the cutoff used to originally generate the data)", action="store",
+                  type="float", dest="evalue", default=1E-5)
+
+(options, args) = parser.parse_args()
+
+ecc = options.ecc - 1
+
+con = sqlite3.connect(locateDatabase())
+cur = con.cursor()
+
+q = "SELECT * FROM rpsblast_results WHERE external_clusterid = ? AND evalue < ?"
+
+for line in fileinput.input("-"):
+    spl = line.strip("\r\n").split("\t")
+    externalid = spl[ecc]
+    cur.execute(q, (externalid, options.evalue))
+    for res in cur:
+        print "\t".join([ str(s) for s in res ] )

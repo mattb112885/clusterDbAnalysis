700ecdc63fa26ae42bc476d1551d0f979fae3bb3
Matthew Benedict <mattb112885@autorecon.(none)>
2013-03-01 16:49:21 -0600

    More large changes to convertGenbankToTable.py - the current version now automatically places the output files in the correct place (no need for user to specify locations) and refuses to allow multiple organisms with the same ID (user can specify they want to overwrite using -r or can assign a new ID to the converted file using -v)

--- scripts/convertGenbank2table.py
+++ scripts/convertGenbank2table.py
@@ -3,12 +3,14 @@
 
 # Libraries needed
 import fileinput, optparse
-import os, sys, csv, getpass, socket
+import os, sys, csv, getpass, socket, shutil
 
 from Bio import Entrez, SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import generic_protein
+
+from FileLocator import *
 
 '''
 Standards set by Matt, 10/30/2012
@@ -98,14 +100,15 @@ def info_from_record(record):
     #    info["Database cross-references"] = ";".join(record.dbxrefs)
     return info
 
-def genbank_extract(ptr):
-
+def genbank_extract(ptr, version_number):
+    '''Extract data from a genbank file...returns some organism-specific data,
+    a dictionary from gene ID to a list of aliases to that gene ID (including the
+    original IDs from the genbank file), and the data needed to build the raw data
+    table required by ITEP.'''
     #get data
     gb_seqrec_multi = SeqIO.parse(ptr, "genbank")
     #loop, as there may be multple genbanks together (although this is non-canonical)
     #lists to store extracted seqs
-    nfastas = []
-    pfastas = []
     genes = []
     geneidToAlias = {}
     for gb_seqrec in gb_seqrec_multi:
@@ -113,6 +116,7 @@ def genbank_extract(ptr):
         for feature in gb_seqrec.features:
             if feature.type =="CDS":
                 #check there is only one translation and get info
+                #TODO - Should we attempt to translate manually if no translations are preent in the gbk file?
                 if 'translation' not in feature.qualifiers:
                     sys.stderr.write("WARNING: CDS found with no translation\n")
                     sys.stderr.write("Qualifiers:\n")
@@ -128,7 +132,7 @@ def genbank_extract(ptr):
                 geneinfo.update(info_from_record(record))
                 #build output with custom fields (and add them to info list)
                 xrefdict = dict([xref.split(':') for xref in feature.qualifiers['db_xref']])
-                geneid = "fig|" + str(orginfo["taxon"]) + ".88888" + ".peg." + xrefdict['GI'] #the 1 is arbitrary
+                geneid = "fig|" + str(orginfo["taxon"]) + "." + str(version_number) + ".peg." + xrefdict['GI'] #the 1 is arbitrary
                 geneinfo["feature_id"] = geneid
                 geneinfo["location"] = orginfo["gi"]
                 geneinfo["contig_id"] = orginfo["id"]
@@ -145,19 +149,6 @@ def genbank_extract(ptr):
                     aliases.append(feature.qualifiers["locus_tag"][0])
                 geneidToAlias[geneid] = aliases
 
-                #Save in list to writeout
-                precord = SeqRecord(Seq(geneinfo["aa_sequence"], generic_protein),
-                                    name=genename,
-                                    id = geneid,
-                                    description = genedesc
-                                    )
-                pfastas.append(precord)
-                nrecord = SeqRecord(record.seq,
-                                    name=genename,
-                                    id = geneid,
-                                    description = genedesc
-                                    )
-                nfastas.append(nrecord)
                 genes.append(geneinfo)
 
     return orginfo, genes, geneidToAlias
@@ -224,6 +215,7 @@ def fasta_to_fastas(name, table, i):
     #writeout sequences and lists
     SeqIO.write(nfastas, open(nfasta_filename,"w"), "fasta")
     SeqIO.write(pfastas, open(pfasta_filename,"w"), "fasta")
+
     return orginfo, genes
 
 def findtypes(info):
@@ -247,65 +239,131 @@ if email == None:
 Entrez.email = email
 
 if __name__ == '__main__':
-    usage="%prog [options] < genbank_file > tab_delimited_file"
-    description='''    The purpose of this script is to take in a standard Genbank file (.gbk) and output
-    the taboluar files that are required for input into ITEP and make sure all the gene IDs
-    and everything else is formatted correctly.
+    usage="%prog [options] -g genbank_file"
+    description='''    The purpose of this script is to take in a Genbank file (.gbk) 
+    with multiple contig genbank files concatinated and 
+    automatically organize three pieces of information required for input into ITEP:
 
-    The script will create at least two files with names dependent on the taxID within the genbank file.
-    The .gbk file is just a copy of the existing genbank (for now) and should be placed in the genbank/ directory
-    with the provided new name.. The .txt file should be placed in the raw/ directory.
-   '''
+    1: A genbank file with the coorrect name reflecting the taxID and a version number
+    2: A tab-delimited file with the required format (see documentaion in the header of this file)
+    3: An augmented aliases file containing locus tag and geneId information from the genbank file if available.
+
+    You should NOT put your original genbank file in the $ROOT/genbank/ folder - instead put it somewhere else and call this
+    script. This script will automatically reformat it and put the reformatted file into the /genbank/ folder.
+
+    The genbank file is placed in $ROOT/genbank/, the tab-delimited file in $ROOT/raw/ and the aliases
+    file is appended to any existing aliase file in the location $ROOT/aliases/aliases
+
+    '''
     parser = optparse.OptionParser(usage=usage, description=description)
-    parser.add_option("-a", "--alias_file", help="A file to which to write aliases (D: Not written). WARNING: This could create duplicate entries in your ALIASES file!",
-                      action="store", type="str", dest="alias_file", default=None)
+    parser.add_option("-g", "--genbank_file", help="Input genbank file concatinated across contigs (REQUIRED)", action="store", type="str", dest="genbank_file", default=None)
     parser.add_option("-o", "--org_file", help="(OPTIONAL) a file to which to dump organism data.", action="store", type="str", dest="org_file", default=None)
-
+    parser.add_option("-r", "--replace", 
+                      help="If specified, replaces old data related to the derived organism ID with the new data (D: Throws an error if an organism already is present with the derived organism ID)",
+                      action="store_true", dest="replace", default=False)
+    parser.add_option("-v", "--version_number", help="The second number in the \d+\.\d+ format of the organism ID - use this to distinguish between multiple genbank files with the same taxID (D:88888)",
+                      action="store", type="int", dest="version_number", default=88888)
     (options, args) = parser.parse_args()
+    
+    if options.genbank_file is None:
+        sys.stderr.write("ERROR: Genbank_file (-g) is a required argument\n")
+        exit(2)
 
-    geneinfos = []
-    orginfos = []
-    orginfo, genes, aliases = genbank_extract(sys.stdin)
-    orginfos.append(orginfo)
-    geneinfos.append(genes)
-    #write out all genes, with columns in correct order. The comments are where they originate from
+    orginfo, genes, aliases = genbank_extract(options.genbank_file, options.version_number)
+
+    rootdir = locateRootDirectory()
+    organism_id = str(orginfo["taxon"]) + "." + str(options.version_number)
+    geneout_filename = os.path.join(rootdir, "raw", "%s.txt" %(organism_id))
+    genbank_filename = os.path.join(rootdir, "genbank", "%s.gbk" %(organism_id))
+    alias_filename = os.path.join(rootdir, "aliases", "aliases")
+
+    # Try to prevent conflics between multiple organisms with the same taxID
+    if os.path.exists(geneout_filename):
+        if options.replace:
+            # Note - to make this really robusst I should probably add a timestamp or something...
+            bkgeneout_filename = os.path.join(rootdir, "%s.txt.bk" %(organism_id))
+            sys.stderr.write("""WARNING: Backing up original gene output file %s to location %s in case something went wrong\n""" %(geneout_filename, bkgeneout_filename))
+            shutil.copyfile(geneout_filename, bkgeneout_filename)
+        else:
+            sys.stderr.write("""ERROR: Gene output file %s already exists!
+This could indicate a conflict in taxIDs between multiple organisms. Use -r to override this error and 
+replace the existing file with a new one and remove the existing file, or use a different version number (-v)
+if the genomes are really different.\n""" %(geneout_filename))
+            exit(2)
+
+    if os.path.exists(genbank_filename) and not options.replace:
+        if options.replace:
+            # Note - to make this really robusst I should probably add a timestamp or something...
+            bkgenbank_filename = os.path.join(rootdir, "%s.txt.bk" %(organism_id))
+            sys.stderr.write("""WARNING: Backing up original gene output file %s to location %s in case something went wrong\n""" %(genbank_filename, bkgenbank_filename))
+            shutil.copyfile(geneout_filename, bkgenbank_filename)
+        else:
+            sys.stderr.write("""ERROR: Genbank output file %s already exists!
+This could indicate a conflict in taxIDs between multiple organisms. Use -r to override this error and 
+replace the existing file with a new one and remove the existing file, or use a different version number (-v
+if the genomes are really different.\n""" %(genbank_filename))
+            exit(2)
+
+    if os.path.exists(alias_filename):
+        # Lets check to see if we can find our organism in the existing alias file
+        bk_file = os.path.join(rootdir, "aliases", "aliases.bk")
+        shutil.copyfile(alias_filename, bk_file)
+        alias_ptr = open(alias_filename, "w")
+        # The entire organism ID must match and we must avoid subsets.
+        tosearch = "|" + organism_id + "."
+        for line in open(bk_file, "r"):
+            if tosearch in line:
+                if not options.replace:
+                    sys.stderr.write("""ERROR: Entries with gene ID %s already exist in the aliases file.
+This could indicate a conflict in taxIDs between multiple organisms. Use -r to override this error and
+replace the existing entries in the aliases file with new entries or use a different version number (v) if the
+genomes are really different.""")
+                    alias_ptr.close()
+                    os.rename(bk_file, alias_filename)
+                    exit(2)
+                else:
+                    continue
+            else:
+                alias_ptr.write(line)
+        alias_ptr.close()
+
+    # Lets re-write the genbank file first.
+    shutil.copyfile(options.genbank_file, genbank_filename)
+
+    # Now we generate a tab-delimited file with the following fields:
     names = ["contig_id",          #from gi of gb file
-    "feature_id",         #taxonID from NCBI lookup of acention, geneID from the db_xref GI in feature
+    "feature_id",         #taxonID from NCBI lookup of accession, geneID from the db_xref GI in feature
     "type",               #CDS = peg
-    "location",           #ignored by iTEP, source assention here
+    "location",           #ignored by iTEP, source accession here
     "start",              #+1 index and rev if on neg strand
     "stop",               #+1 index and rev if on neg strand
     "strand",             #converted to + or -
     "function",           #from feature product
-    "aliases",            #ignored by iTEP, from gene asention
+    "aliases",            #ignored by iTEP, from gene accession
     "figfam",             #ignored by iTEP, empty for all
     "evidence_codes",     #ignored by iTEP, empty for all
     "nucleotide_sequence",#as recorded in feature
     "aa_sequence"]        #as recorded in feature, not translated manually
 
-    geneout_filename = str(orginfo["taxon"]) + ".88888.txt"
-
     geneout_file = open(geneout_filename, 'w')
     geneout = csv.DictWriter(geneout_file, fieldnames = names, delimiter="\t")
-    #geneout.writeheader()
-    for orggenes in geneinfos:
-        for gene in orggenes:
-            geneout.writerow(dict([(n, gene[n]) for n in names]))
+        #geneout.writeheader()
+
+    for gene in genes:
+        geneout.writerow(dict([(n, gene[n]) for n in names]))
     geneout_file.close()
     sys.stderr.write("Text file saved as %s\n" % geneout_filename)
 
-    if options.alias_file is not None:
-        if os.path.exists(options.alias_file):
-            raise IOError("ERROR: Specified alias file already exists. Specify a new file so that your data doesn't get overwritten")
-        alias_file = open(options.alias_file, "w")
-        for geneid in aliases:
-            for alias in aliases[geneid]:
-                alias_file.write("%s\t%s\n" %(geneid, alias))
-        alias_file.close()
+    # IMPORTANT: Append, don't use "w" here (we don't want to blow away all the different organism entries...)
+    alias_file = open(alias_filename, "a")
+    for geneid in aliases:
+        for alias in aliases[geneid]:
+            alias_file.write("%s\t%s\n" %(geneid, alias))
+    alias_file.close()
 
-    #write out all organism, with the original data columns followed by columns in alphabetical order
+    #Write out the organism data
     if options.org_file is not None:
-        names = orginfos[0].keys()
+        names = orginfo.keys()
         names.sort()
         orgout_file = open(options.orgout_file, "w")
         orgout = csv.DictWriter(orgout_file, fieldnames = names)

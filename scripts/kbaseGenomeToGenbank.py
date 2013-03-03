b0bff8c17205653f21628d78b7bde48130c40ca3
Matthew Benedict <mattb112885@autorecon.(none)>
2013-03-02 18:49:47 -0600

    Add option to manually add taxID if the automatic generation doesnt work (as is often the case)

--- scripts/kbaseGenomeToGenbank.py
+++ scripts/kbaseGenomeToGenbank.py
@@ -76,7 +76,7 @@ def getFieldFromRelationship(seedRelationship, fieldName, objtype):
         f.append(entry[objidx][fieldName])
     return f
 
-def kbaseGenomeToGenbank(genome_object):
+def kbaseGenomeToGenbank(genome_object, taxid=None):
     '''Convert a KBase genome object into a Genbank file incorporating as much info as we can
     as found in the NCBI genbank files.
 
@@ -91,15 +91,20 @@ def kbaseGenomeToGenbank(genome_object):
     organism_genetic_code = genome_object["genetic_code"]
 
     # Get the TaxID
-    # I  have no idea why the CDMI  isn't working...
-    # This is SO FRUSTRATING because the command-line scripts work just fine.
-    #cdmi_entity = CDMI_EntityAPI(URL)
-    #reldict = cdmi_entity.get_relationship_IsInTaxa(organism_id, [], [], ["id"])
-    #taxidlist = getFieldFromRelationship(reldict, "id", "to")
-    #taxid = taxidlist[0]
+    # If none is specified the user has to provide one (or at least some unique integer, not necessarily a tax ID) for this system to work right.
+    if taxid is None:
+        cdmi_entity = CDMI_EntityAPI(URL)
+        reldict = cdmi_entity.get_relationship_IsInTaxa(organism_id, [], [], ["id"])
+        if reldict is None:
+            sys.stderr.write("ERROR: TaxID for Organism ID %s not found - you will need to specify it manually if you want it\n" %(organism_id))
+            exit(2)
+        else:
+            taxidlist = getFieldFromRelationship(reldict, "id", "to")
+            taxid = taxidlist[0]
 
     annotations = { 'source': organism_name, 'organism': organism_name }
 
+    # Specify contig data and "source" features for each contig (required by the genbank standard)
     contig_to_sequence = {}
     contig_to_feature_data = {}
     for contig in genome_object["contigs"]:
@@ -107,9 +112,8 @@ def kbaseGenomeToGenbank(genome_object):
         qualifiers = {}
         qualifiers["organism"] = organism_name
         qualifiers["mol_type"] = "Genomic DNA"
-        #qualifiers["db_xref"] = "taxon:%s" %(taxid)
-        # We also need to create a "source" feature that will hold the TaxID and some other stuff.
-        # We need the TaxID to built our genome IDs in at least a reasonably-consistent manner.
+        if taxid is not None:
+            qualifiers["db_xref"] = "taxon:%s" %(taxid)
         feature = SeqFeature(FeatureLocation(0, len(contig["dna"])), strand=1, type="source", qualifiers=qualifiers)
         contig_to_feature_data[contig["id"]] = [ feature ]
 
@@ -117,7 +121,6 @@ def kbaseGenomeToGenbank(genome_object):
     # in a genbank file the features in a contig must all be separated.
     # Therefore I have to keep track of them in one step and then create the SeqRecord objects
     # in a separate step.
-
     for feature in genome_object["features"]:
         # FIXME - What do I do with things that have more than one location?
         assert(len(feature["location"]) == 1)
@@ -134,7 +137,6 @@ def kbaseGenomeToGenbank(genome_object):
         else:
             strand = 1
         featurelen = loc[3]
-
         if strand == -1:
             stop = start - featurelen + 1
         else:
@@ -153,8 +155,8 @@ def kbaseGenomeToGenbank(genome_object):
         if "function" in feature:
             qualifiers["product"] = strip_control_characters(feature["function"])
         if feature_type == "CDS" or feature_type == "peg":
-            qualifiers["translation"] = feature["protein_translation"]
             qualifiers["protein_id"] = feature_id
+            qualifiers["translation"] = feature["protein_translation"]
 
         # "RNA" is not an official type in a GENBANK file.
         # We attempt to figure out based on the annotation whether it is a tRNA, rRNA, or other (misc_RNA) RNA.
@@ -176,11 +178,13 @@ def kbaseGenomeToGenbank(genome_object):
         # raw files gets it right.
         feature = SeqFeature(FeatureLocation(seqstart, seqstop), strand=strand, type=feature_type, id=feature_id, qualifiers=qualifiers)
 
+        # Attach the new features to the appropriate contig...
         if contig in contig_to_feature_data:
             contig_to_feature_data[contig].append(feature)
         else:
             contig_to_feature_data[contig] = [ feature ]
 
+    # Create one record for each contig
     records = []
     for contig in contig_to_feature_data:
         seq = Seq(contig_to_sequence[contig], IUPAC.ambiguous_dna)
@@ -198,6 +202,8 @@ if __name__ == '__main__':
     usage = "%prog [Genome_object] > Concatinated_genbank_file"
     description = '''Converts a KBase genome JSON object into a genbank file (each contig gets its own file and then they are concatinated)'''
     parser = optparse.OptionParser(usage=usage, description=description)
+    parser.add_option("-t", "--taxid", help="Taxonomy ID for genome to add to organism (D: Attepmt to look it up but if it fails you will need to specify one)",
+                      action="store", type="int", dest="taxid", default=None)
     (options, args) = parser.parse_args()
     if len(args) < 1:
         sys.stderr.write("ERROR: The JSON genome object is a required argument...\n")
@@ -206,4 +212,4 @@ if __name__ == '__main__':
     # The strict=FALSE is necessary because I had issues saving the JSON files from IRIS
     # without invalid control characters being put in by firefox(??)
     genome_object = json.load(open(args[0], "r"), strict=False)
-    kbaseGenomeToGenbank(genome_object)
+    kbaseGenomeToGenbank(genome_object, taxid=options.taxid)

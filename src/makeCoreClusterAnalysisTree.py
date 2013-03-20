c210eb6eb8491a25db521300d4f04bf72ee8b0c1
Matthew Benedict <benedic1@biome.life.illinois.edu>
2013-03-20 11:01:52 -0500

    Could have sworn I implemented this before but apparently not. Re-implement the exporting of clusters at every node of the tree to an Excel file - now part of makeCoreClusterAnalysisTree.py

--- src/makeCoreClusterAnalysisTree.py
+++ src/makeCoreClusterAnalysisTree.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python
 
+import optparse
+import os
+import sqlite3
+import sys
+
 from ete2 import Tree, TextFace, CircleFace
-import optparse, os, sys
+
 from CoreGeneFunctions import *
+from ClusterFuncs import *
+from FileLocator import *
 from TreeFuncs import *
 
 usage="%prog [options] newick_file runid"
 description="""Generate an ETE tree with internal node labels corresponding to the number of
 clusters conserved in the nodes beneath it (conservation being defined by a variery of options
-below). The input MUST be a Newick file with organism names replaced."""
+below). The input MUST be a Newick file with organism names replaced.
+
+Alternatively (or in addition) export a XLS file with sheet names equal to the node number printed
+on the tree containing the cluster, runid pair and a representative annotation from each cluster
+identified with these properties..."""
 
 parser = optparse.OptionParser(usage=usage, description=description)
 parser.add_option("-d", "--display", help="Display tree", action="store_true", dest="display", default=False)
 parser.add_option("-v", "--savesvg", help="Save tree as SVG (requires -b)", action="store_true", dest="savesvg", default=False)
 parser.add_option("-p", "--savepng", help="save tree as PNG (implies -v, requires -b)", action="store_true", dest="savepng", default=False)
+parser.add_option("-x", "--savexls", help="Output lists of clusters for each tree to XLS (D: Dont). Requires xlwt.", action="store_true", dest="savexls", default=False)
+parser.add_option("-b", "--basename", help="Output file base name (D: automatically generated)", action="store", dest="basename", default=None)
+
 parser.add_option("-r", "--reroot", help="Reroot tree to specified leaf before doing calculation (D: Use tree as is)",
                   action="store", type="str", dest="reroot_org", default=None)
+
 parser.add_option("-a", "--all", 
                   help="""Count the clusters containing all of the descendent leaves of each internal node. Only specifying this
 option, the program ignores every leaf that is not a descendent of the internal node.""",
@@ -34,7 +49,6 @@ are represented in the cluster)""",
 parser.add_option("-y", "--any",
                   help = "Count clusters if they have representatives in ANY descendent node from each internal node.""",
                   action = "store_true", dest="any", default = False)
-parser.add_option("-b", "--basename", help="Output file base name (D: automatically generated)", action="store", dest="basename", default=None)
 
 (options, args) = parser.parse_args()
 
@@ -42,8 +56,8 @@ if len(args) < 2:
     sys.stderr.write("ERROR: Newick file and runID are required\n")
     exit(2)
 
-if not (options.display or options.savesvg or options.savepng):
-    sys.stderr.write("ERROR: At least One of -v, -p, or -d is required\n")
+if not (options.display or options.savesvg or options.savepng or options.savexls):
+    sys.stderr.write("ERROR: At least One of -v, -p, -x or -d is required\n")
     exit(2)
 
 if options.savepng:
@@ -63,6 +77,8 @@ if options.basename is None:
         truthvalue_str.append("uniq")
     options.basename = "%s_%s" %(args[1], "_".join(truthvalue_str))
 
+con = sqlite3.connect(locateDatabase())
+cur = con.cursor()
 
 t = Tree(args[0])
 runid = args[1]
@@ -77,6 +93,19 @@ t, ts = prettifyTree(t, show_bootstraps = False)
 t, data = addCoreDataToTree(t, runid, 
                             all_org = options.all, any_org = options.any, only_org = options.only, 
                             none_org = options.none, uniq_org = options.uniq)
+
+if options.savexls:
+    import xlwt
+    wb = xlwt.Workbook()
+    for nodenum in data:
+        sheet = wb.add_sheet(str(nodenum))
+        rownum = 0
+        for clusterrun in data[nodenum]:
+            sheet.write(rownum, 0, clusterrun[0])
+            sheet.write(rownum, 1, clusterrun[1])
+            sheet.write(rownum, 2, findRepresentativeAnnotation(clusterrun[0], clusterrun[1], cur))
+            rownum += 1
+    wb.save("%s.xls" %(options.basename) )        
 
 if options.savesvg:
     # Some versions of ETE create a "test.svg" and others do not.

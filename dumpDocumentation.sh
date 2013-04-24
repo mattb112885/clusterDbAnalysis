3337e0509a0504b53a2498f4c23bf76c0d35c6e3
Matthew Benedict <mattb112885@autorecon.(none)>
2013-04-24 13:47:38 -0500

    Update documentation dumper to include utilities directory

--- dumpDocumentation.sh
+++ dumpDocumentation.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Note - we only dump documentation for the scripts we expect people to use (i.e. not things in src/internal/)
+
 mkdir "doc" 2> /dev/null
 rm doc/help_texts
+
+echo "o----------------------------------------------------------------------------------------------------------o" >> doc/help_texts
+echo "|                                       COMMAND-LINE FUNCTIONS                                             |" >> doc/help_texts
+echo "o----------------------------------------------------------------------------------------------------------o" >> doc/help_texts
+echo "" >> doc/help_texts
 
 cd src;
 for file in *.py; do
@@ -20,7 +27,24 @@ for file in *.sh; do
     sh ${file} >> ../doc/help_texts
 done
 
+cd utilities;
+
+for file in *.py; do
+    echo "*--------------------------------------------------------------------------------------------------------------------------*" >> ../../doc/help_texts
+    echo "" >> ../../doc/help_texts
+    echo "${file}" >> ../../doc/help_texts
+    echo "" >> ../../doc/help_texts
+    ${file} -h >> ../../doc/help_texts
+done
+
 cd ..;
+cd ..;
+
+echo "" >> doc/help_texts
+echo "o----------------------------------------------------------------------------------------------------------o" >> doc/help_texts
+echo "|                                       CONVENIENCE SCRIPTS                                                |" >> doc/help_texts
+echo "o----------------------------------------------------------------------------------------------------------o" >> doc/help_texts
+echo "" >> doc/help_texts
 
 cd scripts;
 

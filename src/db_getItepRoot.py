c277ecae0a2b077ab6d32dd3191ca8636d30713c
Matthew Benedict <mattb112885@autorecon.(none)>
2013-04-24 12:49:26 -0500

    Add function to get root directory of current ITEP database

---
+++ src/db_getItepRoot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import optparse
+
+from FileLocator import *
+
+usage = "%prog"
+description = "Get the current root directory of the TIEP repository"
+parser = optparse.OptionParser(usage=usage, description=description)
+parser.parse_args()
+
+print locateRootDirectory()

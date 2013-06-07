318f7439408e0ab371870ac96a572ceb09cf90e7
Matthew Benedict <benedic1@biome.life.illinois.edu>
2013-06-07 17:01:47 -0500

    Function to get external cluster data if all you have is the ID of that external cluster

---
+++ src/db_getExternalClustersById.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import fileinput
+import optparse
+import sqlite3
+import sys
+
+from FileLocator import *
+
+usage = "%prog [options] < external_clusterids > descriptions"
+description = "Get descriptions of external clusters described by the specified external clusterIDs"
+parser = optparse.OptionParser(usage=usage, description=description)
+parser.add_option("-c", "--column", help="Column number (starting from 1) for the external cluster ID (D=1)", 
+                  action="store", type="int", dest="idc", default=1)
+(options, args) = parser.parse_args()
+
+idc = options.idc - 1
+
+con = sqlite3.connect(locateDatabase())
+cur = con.cursor()
+query = """SELECT * FROM external_clusters WHERE external_clusterid = ?"""
+
+for line in fileinput.input("-"):
+    spl = line.strip("\r\n").split("\t")
+    eid = spl[idc]
+    cur.execute(query, (eid, ))
+    for res in cur:
+        print "\t".join([str(s) for s in res])

9feb6ec173cc06f61f1e3d07214a19cedec2d6b4
Matthew Benedict <benedic1@biome.life.illinois.edu>
2013-06-12 10:29:40 -0500

    Fix issue with NULL processing in main5

--- main5.sh
+++ main5.sh
@@ -4,16 +4,18 @@
 #
 # Load user-specifc data into the database
 
-if [ ! -f /userfiles/user_genes ]; then
-    echo "Main5.sh is intended for loading the user_genes file (located in userfules/)"
+if [ ! -f userdata/user_genes ]; then
+    echo "Main5.sh is intended for loading the user_genes file (located in userdata/)"
     echo "No user_genes file was found so this command is a no-op."
     echo ""
     exit 1;
 fi
 
 # Load user_genes into the database
+echo "Loading user gene data..."
 sqlite3 db/DATABASE.sqlite < src/internal/builddb_5.sql;
 
 # Re-load the presence-absence table with user-defined genes
+echo "Re-generating presence-absence table..."
 db_loadPresenceAbsence.py;
 

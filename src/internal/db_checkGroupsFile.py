32af3f5135b46e2cd2ff8bc6a3ac3d163caca3a5
Matthew Benedict <benedic1@biome.life.illinois.edu>
2013-07-23 13:59:31 -0500

    Check for consistency between groups file organism names and those in the organisms file (or in the database)

---
+++ src/internal/db_checkGroupsFile.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from ClusterFuncs import *
+from FileLocator import *
+import optparse
+import sqlite3
+import sys
+
+usage = "%prog"
+description = "Check whether all of the organisms in the groups file are in the database."
+parser = optparse.OptionParser(usage=usage, description=description)
+(options, args) = parser.parse_args()
+
+groupsfile = locateGroupsFile()
+
+con = sqlite3.connect(locateDatabase())
+cur = con.cursor()
+
+STATUS=0
+for line in open(groupsfile, "r"):
+    spl = line.strip("\r\n").split("\t")
+    groupname = spl[0]
+    organisms = spl[1].split(";")
+    for org in organisms:
+        try:
+            organismNameToId(org, cur, issanitized = False)
+        except ValueError:
+            sys.stderr.write("Organism with name \"%s\" not found in database. Was this a typo? (Note - organism names with semicolons in them will cause problemd)\n" %(org))
+            STATUS=1
+
+exit(STATUS)
+
